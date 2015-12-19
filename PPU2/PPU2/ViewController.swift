be1697cf80cfd28d7d8959ca50ebf7e910d50524
davismony@me.com <davismony@me.com>
2015-12-19 09:57:38 -0800

    Transitioned Item1 with Animation at Launch

--- PPU2/PPU2/ViewController.swift
+++ PPU2/PPU2/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     let myGreen = UIColor(red: 88/255, green: 185/255, blue: 142/255, alpha: 1)
     let myRed = UIColor(red: 228/55, green: 56/255, blue: 68/255, alpha: 1)
     
+    
+    
     @IBOutlet weak var Item1: UITextField!
     
     @IBOutlet weak var Quantity1: UITextField!
@@ -29,10 +31,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var calcButton: UIButton!
     
+    @IBOutlet weak var clearBtn: UIButton!
+    
+    
+    
     @IBAction func oneCalc(sender: UIButton) {
         newCalc()
-        calcButton.alpha = 0.9
-        calcButton.backgroundColor = UIColor.lightGrayColor()
+//        calcButton.alpha = 1
+        UIView.animateWithDuration(0.2) { () -> Void in
+            self.calcButton.backgroundColor = UIColor.lightGrayColor()
+        }
+
     }
     
     // TODO: Set some other customizations
@@ -42,6 +51,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         miniClear()
         calcButton.backgroundColor = myGreen
         calcButton.setTitle("Calculate", forState: UIControlState.Normal)
+        
     }
     
  
@@ -98,14 +108,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
             result1Label.backgroundColor = UIColor.whiteColor()
             result2Label.backgroundColor = UIColor.whiteColor()
         }
+        
+        UIView.animateWithDuration(0.2) { () -> Void in
+            self.result1Label.alpha = 1
+            self.result2Label.alpha = 1
+        }
+        
+        
     }
     
 // reset results when some re-edit is taking place.
     func miniClear() {
-        result1Label.text = ""
-        result2Label.text = ""
-        result1Label.backgroundColor = UIColor.clearColor()
-        result2Label.backgroundColor = UIColor.clearColor()
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.result1Label.alpha = 0
+            self.result2Label.alpha = 0
+        }
+//        result1Label.backgroundColor = UIColor.clearColor()
+//        result2Label.backgroundColor = UIColor.clearColor()
     }
     
 
@@ -116,12 +135,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
         Item2.text = ""
         Quantity1.text = ""
         Quantity2.text = ""
-        result1Label.text = ""
-        result2Label.text = ""
-        result1Label.backgroundColor = UIColor.clearColor()
-        result2Label.backgroundColor = UIColor.clearColor()
         calcButton.setTitle("Calculate", forState: UIControlState.Normal)
-        Item1.becomeFirstResponder()        
+        Item1.becomeFirstResponder()
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.result1Label.alpha = 0
+            self.result2Label.alpha = 0
+            
+        }
+        
     }
     
     @IBAction func ClearAllHit(sender: UIButton) {
@@ -129,23 +150,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
     }
     
-
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         Item1.delegate = self
         Quantity1.delegate = self
         Item2.delegate = self
         Quantity2.delegate = self
+        Item1.center.x = self.view.frame.width - 200
+//        clearBtn.center.x = self.view.frame.width - 200
+
+        UIView.animateWithDuration(1.2, delay: 0.5, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.0, options: .CurveEaseOut, animations: { () -> Void in
+            self.Item1.center.x = self.view.frame.width + 200
+//            self.clearBtn.center.x = self.view.frame.width + 200
+
+            
+            //            self.calcButton.alpha = 1.0
+            }, completion: nil)
+        
+        Item1.becomeFirstResponder()
+        //        calcButton.alpha = 0
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    
 
 }
 
